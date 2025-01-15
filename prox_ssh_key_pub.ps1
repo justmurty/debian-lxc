85755fbe99759f78eff29400589bc8febc1e8dc7
Muraza Yeni <murti.yeni01@gmail.com>
2025-01-15 08:32:36 +0200

    Update prox_ssh_key_pub.ps1

--- prox_ssh_key_pub.ps1
+++ prox_ssh_key_pub.ps1
@@ -44,7 +44,7 @@ try {
 
 # Execute the Bash script from GitHub
 $GitHubURL = "https://raw.githubusercontent.com/justmurty/proxmox-ssh_pub-add/refs/heads/win/ln.sh"
-$RemoteCommand = "bash -c \$(wget -qO- $GitHubURL)"
+$RemoteCommand = "bash -c \\\$(wget -qO- $GitHubURL)\\\"
 
 Write-Color "Executing the script on Proxmox..." "Yellow"
 try {
